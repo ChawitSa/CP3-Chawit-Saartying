a1b5a8c6eabb9dab1bd388f8a625b0cb8d56f437
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-01 22:43:46 +0700

    Edit

--- Exercise4_Chawit_S.py
+++ Exercise4_Chawit_S.py
@@ -3,8 +3,11 @@ class Score:
         self.fndEng = fe
         self.genBus = gb
         self.comSys = cs
-        self.ComPro = cp
+        self.comPro = cp
 
 s1 = Score(100, 99, 98, 97)
 print("--- Your Score ---")
-print("Foundation English : ", s1.fndEng)
+print("Foundation English :\t\t\t", s1.fndEng,
+      "\nGeneral Business :\t\t\t", s1.genBus,
+      "\nIntroduction to Computer System :\t", s1.comSys,
+      "\nComputer Programming :\t\t\t",s1.comPro)
