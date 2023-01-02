ff58700f064e79084bc7844b373bb51dfbfec17d
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-02 22:17:52 +0700

    Update

--- TTest.py
+++ TTest.py
@@ -5,10 +5,9 @@ with open('CP3_csv.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            row.replace("\uFEFF", "")
             print('Column names are ', (", ".join(row)))
             line_count += 1
         else:
             print('\t',row[0],' like ',row[1],' and favorite pet is ',row[2],'.')
             line_count += 1
-    print(f'Processed {line_count} {row[0]} lines.')
+    print(f'Processed {line_count} lines.')
