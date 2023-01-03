586d47fdc1474a25ead39a7b70e99f55b92ac0f7
ChawitSa <Chawit.Sa@mail.wu.ac.th>
2023-01-03 07:55:06 +0700

    update

--- TTest.py
+++ TTest.py
@@ -1,13 +1,4 @@
-import csv
+import tkinter
 
-with open('CP3_csv.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print('Column names are ', (", ".join(row)))
-            line_count += 1
-        else:
-            print('\t',row[0],' like ',row[1],' and favorite pet is ',row[2],'.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+mainWindow = tkinter.Tk()
+mainWindow.mainloop()
